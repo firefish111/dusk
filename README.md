da0456e88540a4a54a08d4ed3dc12103e5a86f7e
John Stevonson <mailsodd@gmail.com>
2020-09-01 14:24:57 +0000

    It works!

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dusk
 A package manager for @DynamicSquid's Language Night.
+# examples:
+```
+> dusk add example
+Installed package example v1.4
+> dusk del example
+Uninstalled package example 
+> dusk add example
+Installed package example v1.4
--- dusk.go
+++ dusk.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-  "net/http"
+	"net/http"
 	"os"
 )
 
@@ -18,37 +18,42 @@ func main() {
 		fmt.Fprintf(os.Stderr, "dusk usage: %s command package [more packages]\n", os.Args[0]) // incorrectly formatted
 		os.Exit(1)
 	} else if os.Args[1] == "help" {
-		fmt.Printf("Usage: %s command package [more packages]\nExample:\n\n\t%s add myPkg\nThe above installs myPkg.\n\n\t%s del myPkg\nThe above deletes myPkg.\n\n\t%s inf myPkg\nThe above shows information about myPkg.\n", os.Args[0], os.Args[0], os.Args[0], os.Args[0]) // prints help
+		fmt.Printf("Usage: %s command package [more packages]\nExample:\n\n\t%s add myPkg\nThe above installs myPkg.\n\n\t%s del myPkg\nThe above deletes myPkg.\n", os.Args[0], os.Args[0], os.Args[0], os.Args[0]) // prints help
 		os.Exit(0)
 	}
-	for _, pkg := range os.Args[2:] { // iterates over all the packkages passed
-		client := &http.Client{} // creates client
+	for _, pkg := range os.Args[2:] { // iterates over all the packages passed
+    if os.Args[1] != "del" {
+      client := &http.Client{} // creates client
 
-		res, err := client.Get("https://duskcdn.firefish.repl.co/cdn/" + pkg)
-		req, err := http.NewRequest("GET", "https://duskcdn.firefish.repl.co/cdn/"+pkg, nil) // initialise request
-		safe(err)
+      res, err := client.Get("https://duskcdn.firefish.repl.co/cdn/" + pkg)
+      req, err := http.NewRequest("GET", "https://duskcdn.firefish.repl.co/cdn/"+pkg, nil) // initialise request
+      safe(err)
 
-		req.Header.Add("X-Requested-With", "night-dusk-pkg")
-		res, err = client.Do(req) // send request
-		safe(err)
+      req.Header.Add("X-Requested-With", "night-dusk-pkg")
+      res, err = client.Do(req) // send request
+      safe(err)
 
-		body, err := ioutil.ReadAll(res.Body) // read body of response
-		res.Body.Close()
-		safe(err)
+      body, err := ioutil.ReadAll(res.Body) // read body of response
+      res.Body.Close()
+      safe(err)
 
-    fmt.Printf("\x1b[1m\x1b[38;5;164mInstalling \x1b[38;5;202m%s\x1b[38;5;155mv", pkg)
-    for indx, err := range res.Header["X-Package-Version"] {
-      fmt.Printf("%s", err)
-      if indx != len(res.Header["X-Package-Version"])-1 {
-        fmt.Print(".")
+      os.Mkdir("./dusk_modules", 0755)
+      if os.Args[1] == "add" {
+        fmt.Printf("\x1b[1m\x1b[38;5;164mInstalled package \x1b[38;5;202m%s \x1b[38;5;155mv", pkg)
+        for indx, err := range res.Header["X-Package-Version"] {
+          fmt.Printf("%s", err)
+          if indx != len(res.Header["X-Package-Version"])-1 {
+            fmt.Print(".")
+          }
+        }
+        fmt.Print("\x1b[0m\n")
+        err = ioutil.WriteFile("./dusk_modules/"+pkg+".night", body, 0666)
+        safe(err) // create night file
       }
-    }
-    fmt.Print("\x1b[0m\n")
-
-		os.Mkdir("./dusk_modules", 0755) 
-		err = ioutil.WriteFile("./dusk_modules/"+pkg+".night", body, 0666)
-		safe(err) // create night file
-
-		fmt.Printf("%s", body) // echo night file to console: debug feature
+    } else {
+      fmt.Printf("\x1b[1m\x1b[38;5;164mUninstalled package \x1b[38;5;202m%s \x1b[38;5;155m\n", pkg)
+			err := os.Remove("./dusk_modules/" + pkg + ".night") // delete night file
+			safe(err)
+		}
 	}
 }
