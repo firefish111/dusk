6c40354926e89baf7eca67d080c8069b2888825a
AntimatterReactor <ezraalvarion71@gmail.com>
2022-02-04 19:48:25 +0700

    Reformat code using go fmt

--- dusk.go
+++ dusk.go
@@ -1,162 +1,163 @@
 package main
-import (
-  "encoding/json"
-  "fmt"
-  "io/ioutil"
-  "io/fs"
-  "net/http"
-  "os"
-  "strings"
-  "errors"
 
-  // haha chinese package go brrrrrrr
-  "github.com/gookit/color"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	// haha chinese package go brrrrrrr
+	"github.com/gookit/color"
 )
 
 // safety go brrrrrr
 func safe(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
 // long boi function chain
 func vparse(ver interface{}) string {
-  return strings.Trim(
-    strings.Join(
-      strings.Fields(
-        fmt.Sprint(ver),
-      ), ".",
-    ), "[]",
-  )
+	return strings.Trim(
+		strings.Join(
+			strings.Fields(
+				fmt.Sprint(ver),
+			), ".",
+		), "[]",
+	)
 }
 
-
 func main() {
-  meta := make(map[string]map[string]interface{})
-  // not paying attention to error, because am smort
-  os.Mkdir("./pkgs", 0755)
+	meta := make(map[string]map[string]interface{})
+	// not paying attention to error, because am smort
+	os.Mkdir("./pkgs", 0755)
 
-  // haha colours go brrrrrr
-  indigo := color.C256(63, false)
-  lime   := color.C256(155, false)
-  pink   := color.C256(164, false)
-  orange := color.C256(202, false)
-  bold   := color.Style{color.Bold}
-  rederr := color.Style{color.FgRed}
+	// haha colours go brrrrrr
+	indigo := color.C256(63, false)
+	lime   := color.C256(155, false)
+	pink   := color.C256(164, false)
+	orange := color.C256(202, false)
+	bold   := color.Style{color.Bold}
+	rederr := color.Style{color.FgRed}
 
-  // if not exist
-  if _, err := os.Stat("./pkgs/metadata.json"); !errors.Is(err, fs.ErrNotExist) {
-    // readfile
-    dat, err := ioutil.ReadFile("./pkgs/metadata.json")
-    safe(err)
-    // put contents of file in meta, parsed as json
-    err = json.Unmarshal([]byte(dat), &meta)
-    safe(err)
-  }
+	// if not exist
+	if _, err := os.Stat("./pkgs/metadata.json"); !errors.Is(err, fs.ErrNotExist) {
+		// readfile
+		dat, err := ioutil.ReadFile("./pkgs/metadata.json")
+		safe(err)
+		// put contents of file in meta, parsed as json
+		err = json.Unmarshal([]byte(dat), &meta)
+		safe(err)
+	}
 
-  if len(os.Args) == 1 || (len(os.Args) <= 2 && os.Args[1] != "ls") {
-    // usage
-    fmt.Fprintln(os.Stderr, "usage: dusk command package [more packages]")
-    os.Exit(1)
-  } else if os.Args[1] == "ls" {
-    // listing packages
-    if len(meta) == 0 {
-      indigo.Println("Unfortunately, no packages are installed.")
-    } else {
-      indigo.Println("Installed packages are:")
-      for ky := range meta {
-        bold.Print(pink.Sprint("Package "))
-        orange.Printf("%s ", ky)
-        lime.Printf("v%s\n", vparse(meta[ky]["version"]))
-      }
-    }
-    os.Exit(0)
-  }
-  for _, pkg := range os.Args[2:] { // iterates over all the packages passed
-    if os.Args[1] == "del" {
-      err := os.Remove("./pkgs/" + pkg + ".night") // delete night file
-      safe(err)
-      bold.Print(pink.Sprint("Uninstalled package "))
-      orange.Printf("%s ", pkg)
-      lime.Printf("v%s\n", vparse(meta[pkg]["version"]))
+	if len(os.Args) == 1 || (len(os.Args) <= 2 && os.Args[1] != "ls") {
+		// usage
+		fmt.Fprintln(os.Stderr, "usage: dusk command package [more packages]")
+		os.Exit(1)
+	} else if os.Args[1] == "ls" {
+		// listing packages
+		if len(meta) == 0 {
+			indigo.Println("Unfortunately, no packages are installed.")
+		} else {
+			indigo.Println("Installed packages are:")
+			for ky := range meta {
+				bold.Print(pink.Sprint("Package "))
+				orange.Printf("%s ", ky)
+				lime.Printf("v%s\n", vparse(meta[ky]["version"]))
+			}
+		}
+		os.Exit(0)
+	}
+	for _, pkg := range os.Args[2:] { // iterates over all the packages passed
+		if os.Args[1] == "del" {
+			err := os.Remove("./pkgs/" + pkg + ".night") // delete night file
+			safe(err)
+			bold.Print(pink.Sprint("Uninstalled package "))
+			orange.Printf("%s ", pkg)
+			lime.Printf("v%s\n", vparse(meta[pkg]["version"]))
 
-      delete(meta, pkg)
-    } else {
-      client := &http.Client{} // creates client
+			delete(meta, pkg)
+		} else {
+			client := &http.Client{} // creates client
 
-      // res, err := client.Get("https://duskcdn.firefish.repl.co/cdn/" + pkg)
-      var method string
-      if method = "GET"; os.Args[1] == "inf" {
-        method = "POST"
-      }
-      req, err := http.NewRequest(method, fmt.Sprintf(
-        "https://duskcdn.firefish.repl.co/cdn/%s", pkg,
-      ), nil) // initialise request
-      safe(err)
+			// res, err := client.Get("https://duskcdn.firefish.repl.co/cdn/" + pkg)
+			var method string
+			if method = "GET"; os.Args[1] == "inf" {
+				method = "POST"
+			}
+			req, err := http.NewRequest(method, fmt.Sprintf(
+				"https://duskcdn.firefish.repl.co/cdn/%s", pkg,
+			), nil) // initialise request
+			safe(err)
 
-      req.Header.Add("X-Requested-With", "night-dusk-pkg")
-      res, err := client.Do(req) // send request
-      safe(err)
-      if res.StatusCode != 200 {
-              panic(fmt.Sprintf("Status Code %d", res.StatusCode))
-              os.Exit(1)
-      }
+			req.Header.Add("X-Requested-With", "night-dusk-pkg")
+			res, err := client.Do(req) // send request
+			safe(err)
+			if res.StatusCode != 200 {
+				panic(fmt.Sprintf("Status Code %d", res.StatusCode))
+				os.Exit(1)
+			}
 
-      if os.Args[1] != "inf" {
-        body, err := ioutil.ReadAll(res.Body) // read body of response
-        res.Body.Close()
-        safe(err)
-        switch os.Args[1] {
-        case "upd":
-          if _, err = os.Stat("./pkgs/" + pkg + ".night"); !os.IsNotExist(err) {
+			if os.Args[1] != "inf" {
+				body, err := ioutil.ReadAll(res.Body) // read body of response
+				res.Body.Close()
+				safe(err)
+				switch os.Args[1] {
+				case "upd":
+					if _, err = os.Stat("./pkgs/" + pkg + ".night"); !os.IsNotExist(err) {
 
-            err = ioutil.WriteFile("./pkgs/"+pkg+".night", body, 0666)
-            safe(err) // write to night file
+						err = ioutil.WriteFile("./pkgs/"+pkg+".night", body, 0666)
+						safe(err) // write to night file
 
-            if vparse(res.Header["X-Package-Version"]) == vparse(meta[pkg]["version"]) {
-              fmt.Fprintln(os.Stderr, rederr.Sprintf("Package %s already at latest version.", pkg))
-              os.Exit(0)
-            }
+						if vparse(res.Header["X-Package-Version"]) == vparse(meta[pkg]["version"]) {
+							fmt.Fprintln(os.Stderr, rederr.Sprintf("Package %s already at latest version.", pkg))
+							os.Exit(0)
+						}
 
-            bold.Print(pink.Sprint("Updated package "))
-            orange.Printf("%s ", pkg)
-            lime.Printf("v%s to v%s\n",
-              vparse(meta[pkg]["version"]),
-              vparse(res.Header["X-Package-Version"]),
-            )
+						bold.Print(pink.Sprint("Updated package "))
+						orange.Printf("%s ", pkg)
+						lime.Printf("v%s to v%s\n",
+							vparse(meta[pkg]["version"]),
+							vparse(res.Header["X-Package-Version"]),
+						)
 
-            meta[pkg]["version"] = res.Header["X-Package-Version"]
-            break
-          }
-          fmt.Fprintln(os.Stderr, rederr.Sprint("Warning: destination file doesn't exist, installing package instead"))
-          fallthrough
-        case "add":
-          if _, err = os.Stat("./pkgs/" + pkg + ".night"); !os.IsNotExist(err) {
-            panic(fmt.Sprintf("File already exists, please use %s",
-              lime.Sprint("dusk upd **[packages]**"),
-            ))
-          }
+						meta[pkg]["version"] = res.Header["X-Package-Version"]
+						break
+					}
+					fmt.Fprintln(os.Stderr,
+						rederr.Sprint("Warning: destination file doesn't exist, installing package instead"))
+					fallthrough
+				case "add":
+					if _, err = os.Stat("./pkgs/" + pkg + ".night"); !os.IsNotExist(err) {
+						panic(fmt.Sprintf("File already exists, please use %s",
+							lime.Sprint("dusk upd **[packages]**"),
+						))
+					}
 
-          err = ioutil.WriteFile("./pkgs/"+pkg+".night", body, 0666)
-          safe(err) // write to night file
+					err = ioutil.WriteFile("./pkgs/"+pkg+".night", body, 0666)
+					safe(err) // write to night file
 
-          bold.Print(pink.Sprint("Installed package "))
-          orange.Printf("%s ", pkg)
-          lime.Printf("v%s\n", vparse(res.Header["X-Package-Version"]))
+					bold.Print(pink.Sprint("Installed package "))
+					orange.Printf("%s ", pkg)
+					lime.Printf("v%s\n", vparse(res.Header["X-Package-Version"]))
 
-          meta[pkg] = make(map[string]interface{})
-          meta[pkg]["version"] = res.Header["X-Package-Version"]
-        }
-      } else {
-        bold.Print(pink.Sprint("Package "))
-        orange.Printf("%s ", pkg)
-        lime.Printf("v%s\n", vparse(res.Header["X-Package-Version"]))
-      }
-    }
-  }
-  dat, err := json.Marshal(meta)
-  safe(err)
-  err = ioutil.WriteFile("./pkgs/metadata.json", dat, 0666)
-  safe(err)
+					meta[pkg] = make(map[string]interface{})
+					meta[pkg]["version"] = res.Header["X-Package-Version"]
+				}
+			} else {
+				bold.Print(pink.Sprint("Package "))
+				orange.Printf("%s ", pkg)
+				lime.Printf("v%s\n", vparse(res.Header["X-Package-Version"]))
+			}
+		}
+	}
+	dat, err := json.Marshal(meta)
+	safe(err)
+	err = ioutil.WriteFile("./pkgs/metadata.json", dat, 0666)
+	safe(err)
 }
